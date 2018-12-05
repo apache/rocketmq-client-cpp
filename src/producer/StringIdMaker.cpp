bfba2d123a7f809d1a5a5fec56ff2561e032bc0d
ShannonDing <libya_003@163.com>
2018-12-05 23:02:55 +0800

    Add licence and notice

--- src/producer/StringIdMaker.cpp
+++ src/producer/StringIdMaker.cpp
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "StringIdMaker.h"
 
 namespace rocketmq {
@@ -100,7 +116,7 @@ uint64_t StringIdMaker::get_curr_ms() {
   gettimeofdayWin(&time_now, NULL); //  WIN32
 #else
   gettimeofday(&time_now, NULL);	//LINUX
-#endif 
+#endif
 
   uint64_t ms_time = time_now.tv_sec * 1000 + time_now.tv_usec / 1000;
   return ms_time;
