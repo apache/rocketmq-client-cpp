4db6d51266a816587acb92630ac17b5f818d5fd1
laohu <2372554140@qq.com>
2018-12-31 03:32:51 +0800

    add ci(travis)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -27,62 +27,65 @@ fname_libevent_down="release-2.0.22-stable.zip"
 fname_jsoncpp_down="0.10.6.zip"
 fname_boost_down="1.58.0/boost_1_58_0.tar.gz"
 
-Help()
+PrintParams()
 {
     echo "=========================================one key build help============================================"
-    echo "sh build.sh [build libevent:0/1 default:1] [build json:0/1 default:1] [build boost:0/1 default:1]"
-    echo "usage: sh build.sh 1 1 1"
-    echo "[[build libevent]: 1: need build libevent lib, 0: no need build libevent lib; default:1]"
-    echo "[[build json]: 1: need build json lib, 0: no need build json lib; default:1]"
-    echo "[[build boost]: 1: need build boost lib, 0: no need build boost lib; default:1]"
+    echo "sh build.sh [no build libevent:noEvent] [no build json:noJson] [no build boost:noBoost] [ execution test:test]"
+    echo "usage: sh build.sh noJson noEvent noBoost test"
     echo "=========================================one key build help============================================"
     echo ""
 }
 
-if [ $# -eq 3 ];then
-    if [ "$1" != "0" -a "$1" != "1" ];then
-        echo "unsupport para value $1, please see the help"
-        Help
-        exit 1
-    fi
-    if [ "$2" != "0" -a "$2" != "1" ];then
-        echo "unsupport para value $2, please see the help"
-        Help
-        exit 1
-    fi
-    if [ "$3" != "0" -a "$3" != "1" ];then
-        echo "unsupport para value $3, please see the help"
-        Help
-        exit 1
-    fi
-elif [ $# -gt 0 ];then
-    echo "the number of parameter must 0 or 3, please see the help"
-    Help
-    exit 1
-fi
+need_build_jsoncpp=1
+need_build_libevent=1
+need_build_boost=1
+test=0
 
-if [ $# -ge 1 ];then
-    need_build_libevent=$1
-else
-    need_build_libevent=1
-fi
+pasres_arguments(){
+    for var in "$@"
+    do
+        case "$var" in
+                noJson)
+                        need_build_jsoncpp=0
+                        ;;
+                noEvent)
+                        need_build_libevent=0
+                        ;;
+                noBoost)
+                        need_build_boost=0
+                        ;;
+                test)
+                       test=1
+        esac
+    done
 
-if [ $# -ge 2 ];then
-    need_build_jsoncpp=$2
-else
-    need_build_jsoncpp=1
-fi
-
-if [ $# -ge 3 ];then
-    need_build_boost=$3
-else
-    need_build_boost=1
-fi
+}
+pasres_arguments $@
 
 PrintParams()
 {
     echo "###########################################################################"
-    echo "need_build_libevent: ${need_build_libevent}, need_build_jsoncpp:${need_build_jsoncpp}, need_build_boost:${need_build_boost}"
+    if [ $need_build_libevent -eq 0 ]
+    then
+        echo "no need build libevent lib"
+    else
+        echo "need build libevent lib"
+    fi
+
+    if [ $need_build_jsoncpp -eq 0 ]
+    then
+        echo "no need build jsoncpp lib"
+    else
+        echo "need build jsoncpp lib"
+    fi
+
+    if [ $need_build_boost -eq 0 ]
+    then
+        echo "no need build boost lib"
+    else
+        echo "need build boost lib"
+    fi
+
     echo "###########################################################################"
     echo ""
 }
@@ -97,7 +100,7 @@ Prepare()
     else
         mkdir -p ${down_dir}
     fi
-    
+
     cd ${basepath}
     if [ -e ${fname_libevent} ]
     then
@@ -113,7 +116,7 @@ Prepare()
     then
         mv -f ${basepath}/${fname_boost} ${down_dir}
     fi
-    
+
     if [ -e ${build_dir} ]
     then
         echo "${build_dir} is exist"
@@ -121,7 +124,7 @@ Prepare()
     else
         mkdir -p ${build_dir}
     fi
-	
+
     if [ -e ${packet_dir} ]
     then
         echo "${packet_dir} is exist"
@@ -140,7 +143,8 @@ Prepare()
 
 BuildLibevent()
 {
-    if [ "${need_build_libevent}" == "0" ];then
+    if [ $need_build_libevent -eq 0 ]
+    then
         echo "no need build libevent lib"
         return 0
     fi
@@ -152,7 +156,7 @@ BuildLibevent()
     else
         wget https://github.com/libevent/libevent/archive/${fname_libevent_down} -O libevent-${fname_libevent_down}
     fi
-    unzip -o ${fname_libevent}
+    unzip -o ${fname_libevent} > libevent.txt 2>&1
     if [ $? -ne 0 ];then
         exit 1
     fi
@@ -178,9 +182,10 @@ BuildLibevent()
     make install
 }
 
+
 BuildJsonCPP()
 {
-    if [ "${need_build_jsoncpp}" == "0" ];then
+    if [ $need_build_jsoncpp -eq 0 ];then
         echo "no need build jsoncpp lib"
         return 0
     fi
@@ -193,7 +198,7 @@ BuildJsonCPP()
     else
         wget https://github.com/open-source-parsers/jsoncpp/archive/${fname_jsoncpp_down} -O jsoncpp-${fname_jsoncpp_down}
     fi
-    unzip -o ${fname_jsoncpp}
+    unzip -o ${fname_jsoncpp} > json.txt 2>&1
     if [ $? -ne 0 ];then
         exit 1
     fi
@@ -203,7 +208,7 @@ BuildJsonCPP()
         exit 1
     fi
     mkdir build; cd build
-    echo "build jsoncpp static #####################"
+    echo "build jsoncpp static ######################"
     cmake .. -DCMAKE_CXX_FLAGS=-fPIC -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=${install_lib_dir}
     if [ $? -ne 0 ];then
         exit 1
@@ -213,11 +218,19 @@ BuildJsonCPP()
         exit 1
     fi
     make install
+
+    if [ ! -f ${install_lib_dir}/lib/libjsoncpp.a ]
+    then
+        echo " ./bin/lib directory is not libjsoncpp.a"
+        cp ${install_lib_dir}/lib/x86_64-linux-gnu/libjsoncpp.a ${install_lib_dir}/lib/
+    fi
+
+
 }
 
 BuildBoost()
 {
-    if [ "${need_build_boost}" == "0" ];then
+    if [ $need_build_boost -eq 0 ];then
         echo "no need build boost lib"
         return 0
     fi
@@ -229,7 +242,7 @@ BuildBoost()
     else
         wget http://sourceforge.net/projects/boost/files/boost/${fname_boost_down}
     fi
-    tar -zxvf ${fname_boost}
+    tar -zxvf ${fname_boost} > boost.txt 2>&1
     boost_dir=`ls | grep boost | grep .*[^gz]$`
     cd ${boost_dir}
     if [ $? -ne 0 ];then
@@ -240,7 +253,8 @@ BuildBoost()
         exit 1
     fi    
     echo "build boost static #####################"
-    ./b2 cflags=-fPIC cxxflags=-fPIC --with-atomic --with-thread --with-system --with-chrono --with-date_time --with-log --with-regex --with-serialization --with-filesystem --with-locale --with-iostreams threading=multi link=static runtime-link=static release install --prefix=${install_lib_dir}
+    pwd
+    ./b2 cflags=-fPIC cxxflags=-fPIC   --with-atomic --with-thread --with-system --with-chrono --with-date_time --with-log --with-regex --with-serialization --with-filesystem --with-locale --with-iostreams threading=multi link=static  release install --prefix=${install_lib_dir} 1>boostMakeLog.txt
     if [ $? -ne 0 ];then
         exit 1
     fi
@@ -249,7 +263,11 @@ BuildBoost()
 BuildRocketMQClient()
 {
     cd ${build_dir}
-    cmake ..
+    if [ $test -eq 0 ];then
+        cmake ..
+    else
+        cmake .. -DRUN_UNIT_TEST=ON
+    fi
     make
     if [ $? -ne 0 ];then
         exit 1
@@ -257,6 +275,85 @@ BuildRocketMQClient()
     #sudo make install
     PackageRocketMQStatic
 }
+
+BuildGoogleTest()
+{
+    if [ $test -eq 0 ];then
+        echo "no need build google test lib"
+        return 0
+    fi
+
+    if [ -f ./bin/lib/libgtest.a ]
+    then
+        echo "libgteest already exist no need build test"
+        return 0
+    fi
+
+    cd ${down_dir}
+    if [ -e release-1.8.1.tar.gz ]
+    then
+        echo "${fname_boost} is exist"
+    else
+        wget https://github.com/abseil/googletest/archive/release-1.8.1.tar.gz
+    fi
+    if [ ! -d "googletest-release-1.8.1" ];then
+        tar -zxvf release-1.8.1.tar.gz > googletest.txt 2>&1
+    fi
+    cd googletest-release-1.8.1
+    mkdir build; cd build
+    echo "build googletest static #####################"
+    cmake .. -DCMAKE_CXX_FLAGS=-fPIC -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=${install_lib_dir}
+    if [ $? -ne 0 ];then
+        exit 1
+    fi    
+    make
+    if [ $? -ne 0 ];then
+        exit 1
+    fi
+    make install
+
+    if [ ! -f ${install_lib_dir}/lib/libgtest.a ]
+    then
+        echo " ./bin/lib directory is not libgtest.a"
+        cp ${install_lib_dir}/lib64/lib* ${install_lib_dir}/lib
+    fi
+}
+
+ExecutionTesting()
+{
+    if [ $test -eq 0 ];then
+        echo "Do not execution test"
+        return 0
+    fi
+    echo "##################  test  start  ###########"
+    cd ${basepath}/test/bin
+    if [ ! -d ../log ]; then
+       mkdir ../log
+    fi
+    for files in `ls -F`
+    do
+        ./$files > "../log/$files.txt" 2>&1
+
+        if [ $? -ne 0 ]; then
+            echo "$files erren"
+            cat ../log/$files.txt
+            return 0
+        fi
+        erren=`grep "FAILED TEST" ../log/$files.txt`
+        
+        if [ -n "$erren" ]; then
+            echo "##################  find erren ###########"
+            cat ../log/$files.txt
+
+            echo "##################  end ExecutionTesting ###########"
+            return
+        else
+            echo "$files success"
+        fi
+    done
+    echo "##################  test  end  ###########"
+}
+
 
 PackageRocketMQStatic()
 {
@@ -266,9 +363,12 @@ PackageRocketMQStatic()
     cp -f librocketmq.a ${install_lib_dir}
 }
 
+
 PrintParams
 Prepare
 BuildLibevent
 BuildJsonCPP
 BuildBoost
+BuildGoogleTest
 BuildRocketMQClient
+ExecutionTesting
