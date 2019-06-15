0b74383ecf3a649c80c77eead1bc4397e3c95f67
James Yin <ywhjames@hotmail.com>
2019-06-15 09:34:03 +0800

    replace io_service with executor_service in TopicPublishInfo.

--- src/producer/TopicPublishInfo.h
+++ src/producer/TopicPublishInfo.h
@@ -17,40 +17,27 @@
 #ifndef __TOPICPUBLISHINFO_H__
 #define __TOPICPUBLISHINFO_H__
 
-#include <boost/asio.hpp>
-#include <boost/asio/io_service.hpp>
-#include <boost/atomic.hpp>
-#include <boost/bind.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/scoped_ptr.hpp>
-#include <boost/thread/thread.hpp>
+#include <atomic>
+#include <mutex>
+
+#include "concurrent/executor.hpp"
+
 #include "Logging.h"
 #include "MQMessageQueue.h"
 
 namespace rocketmq {
+
 //<!************************************************************************/
 class TopicPublishInfo {
  public:
-  TopicPublishInfo() : m_sendWhichQueue(0) {
-    m_async_service_thread.reset(new boost::thread(boost::bind(&TopicPublishInfo::boost_asio_work, this)));
-  }
-
-  void boost_asio_work() {
-    boost::asio::io_service::work work(m_async_ioService);  // avoid async io
-                                                            // service stops
-                                                            // after first timer
-                                                            // timeout callback
-    boost::system::error_code e;
-    boost::asio::deadline_timer t(m_async_ioService, boost::posix_time::seconds(60));
-    t.async_wait(boost::bind(&TopicPublishInfo::op_resumeNonServiceMessageQueueList, this, e, &t));
-    boost::system::error_code ec;
-    m_async_ioService.run(ec);
+  TopicPublishInfo() : m_sendWhichQueue(0), m_scheduledExecutorService(false) {
+    m_scheduledExecutorService.startup();
+    m_scheduledExecutorService.schedule(std::bind(&TopicPublishInfo::op_resumeNonServiceMessageQueueList, this), 60,
+                                        time_unit::seconds);
   }
 
   virtual ~TopicPublishInfo() {
-    m_async_ioService.stop();
-    m_async_service_thread->interrupt();
-    m_async_service_thread->join();
+    m_scheduledExecutorService.shutdown();
 
     m_nonSerivceQueues.clear();
     m_onSerivceQueues.clear();
@@ -59,12 +46,12 @@ class TopicPublishInfo {
   }
 
   bool ok() {
-    boost::lock_guard<boost::mutex> lock(m_queuelock);
+    std::lock_guard<std::mutex> lock(m_queuelock);
     return !m_queues.empty();
   }
 
   void updateMessageQueueList(const MQMessageQueue& mq) {
-    boost::lock_guard<boost::mutex> lock(m_queuelock);
+    std::lock_guard<std::mutex> lock(m_queuelock);
     m_queues.push_back(mq);
     string key = mq.getBrokerName() + UtilAll::to_string(mq.getQueueId());
     m_onSerivceQueues[key] = mq;
@@ -76,15 +63,15 @@ class TopicPublishInfo {
     }
   }
 
-  void op_resumeNonServiceMessageQueueList(boost::system::error_code& ec, boost::asio::deadline_timer* t) {
+  void op_resumeNonServiceMessageQueueList() {
     resumeNonServiceMessageQueueList();
-    boost::system::error_code e;
-    t->expires_from_now(t->expires_from_now() + boost::posix_time::seconds(60), e);
-    t->async_wait(boost::bind(&TopicPublishInfo::op_resumeNonServiceMessageQueueList, this, e, t));
+
+    m_scheduledExecutorService.schedule(std::bind(&TopicPublishInfo::op_resumeNonServiceMessageQueueList, this), 60,
+                                        time_unit::seconds);
   }
 
   void resumeNonServiceMessageQueueList() {
-    boost::lock_guard<boost::mutex> lock(m_queuelock);
+    std::lock_guard<std::mutex> lock(m_queuelock);
     for (map<MQMessageQueue, int64>::iterator it = m_brokerTimerMap.begin(); it != m_brokerTimerMap.end(); ++it) {
       if (UtilAll::currentTimeMillis() - it->second >= 1000 * 60 * 5) {
         string key = it->first.getBrokerName() + UtilAll::to_string(it->first.getQueueId());
@@ -97,7 +84,7 @@ class TopicPublishInfo {
   }
 
   void updateNonServiceMessageQueue(const MQMessageQueue& mq, int timeoutMilliseconds) {
-    boost::lock_guard<boost::mutex> lock(m_queuelock);
+    std::lock_guard<std::mutex> lock(m_queuelock);
 
     string key = mq.getBrokerName() + UtilAll::to_string(mq.getQueueId());
     if (m_nonSerivceQueues.find(key) != m_nonSerivceQueues.end()) {
@@ -112,14 +99,14 @@ class TopicPublishInfo {
   }
 
   vector<MQMessageQueue>& getMessageQueueList() {
-    boost::lock_guard<boost::mutex> lock(m_queuelock);
+    std::lock_guard<std::mutex> lock(m_queuelock);
     return m_queues;
   }
 
-  int getWhichQueue() { return m_sendWhichQueue.load(boost::memory_order_acquire); }
+  int getWhichQueue() { return m_sendWhichQueue.load(std::memory_order_acquire); }
 
   MQMessageQueue selectOneMessageQueue(const MQMessageQueue& lastmq, int& mq_index) {
-    boost::lock_guard<boost::mutex> lock(m_queuelock);
+    std::lock_guard<std::mutex> lock(m_queuelock);
 
     if (m_queues.size() > 0) {
       LOG_DEBUG("selectOneMessageQueue Enter, queue size:" SIZET_FMT "", m_queues.size());
@@ -132,8 +119,8 @@ class TopicPublishInfo {
       }
       if (!lastmq.getBrokerName().empty()) {
         for (size_t i = 0; i < m_queues.size(); i++) {
-          if (m_sendWhichQueue.load(boost::memory_order_acquire) == (numeric_limits<int>::max)()) {
-            m_sendWhichQueue.store(0, boost::memory_order_release);
+          if (m_sendWhichQueue.load(std::memory_order_acquire) == (numeric_limits<int>::max)()) {
+            m_sendWhichQueue.store(0, std::memory_order_release);
           }
 
           if (pos >= m_queues.size())
@@ -142,7 +129,7 @@ class TopicPublishInfo {
           ++m_sendWhichQueue;
           MQMessageQueue mq = m_queues.at(pos);
           LOG_DEBUG("lastmq broker not empty, m_sendWhichQueue:%d, pos:%d",
-                    m_sendWhichQueue.load(boost::memory_order_acquire), pos);
+                    m_sendWhichQueue.load(std::memory_order_acquire), pos);
           if (mq.getBrokerName().compare(lastmq.getBrokerName()) != 0) {
             mq_index = pos;
             return mq;
@@ -152,13 +139,13 @@ class TopicPublishInfo {
         LOG_ERROR("could not find property mq");
         return MQMessageQueue();
       } else {
-        if (m_sendWhichQueue.load(boost::memory_order_acquire) == (numeric_limits<int>::max)()) {
-          m_sendWhichQueue.store(0, boost::memory_order_release);
+        if (m_sendWhichQueue.load(std::memory_order_acquire) == (numeric_limits<int>::max)()) {
+          m_sendWhichQueue.store(0, std::memory_order_release);
         }
 
         ++m_sendWhichQueue;
-        LOG_DEBUG("lastmq broker empty, m_sendWhichQueue:%d, pos:%d",
-                  m_sendWhichQueue.load(boost::memory_order_acquire), pos);
+        LOG_DEBUG("lastmq broker empty, m_sendWhichQueue:%d, pos:%d", m_sendWhichQueue.load(std::memory_order_acquire),
+                  pos);
         mq_index = pos;
         return m_queues.at(pos);
       }
@@ -169,7 +156,7 @@ class TopicPublishInfo {
   }
 
   MQMessageQueue selectOneActiveMessageQueue(const MQMessageQueue& lastmq, int& mq_index) {
-    boost::lock_guard<boost::mutex> lock(m_queuelock);
+    std::lock_guard<std::mutex> lock(m_queuelock);
 
     if (m_queues.size() > 0) {
       unsigned int pos = 0;
@@ -181,8 +168,8 @@ class TopicPublishInfo {
       }
       if (!lastmq.getBrokerName().empty()) {
         for (size_t i = 0; i < m_queues.size(); i++) {
-          if (m_sendWhichQueue.load(boost::memory_order_acquire) == (numeric_limits<int>::max)()) {
-            m_sendWhichQueue.store(0, boost::memory_order_release);
+          if (m_sendWhichQueue.load(std::memory_order_acquire) == (numeric_limits<int>::max)()) {
+            m_sendWhichQueue.store(0, std::memory_order_release);
           }
 
           if (pos >= m_queues.size())
@@ -209,15 +196,15 @@ class TopicPublishInfo {
         return MQMessageQueue();
       } else {
         for (size_t i = 0; i < m_queues.size(); i++) {
-          if (m_sendWhichQueue.load(boost::memory_order_acquire) == (numeric_limits<int>::max)()) {
-            m_sendWhichQueue.store(0, boost::memory_order_release);
+          if (m_sendWhichQueue.load(std::memory_order_acquire) == (numeric_limits<int>::max)()) {
+            m_sendWhichQueue.store(0, std::memory_order_release);
           }
           if (pos >= m_queues.size())
             pos = pos % m_queues.size();
 
           ++m_sendWhichQueue;
           LOG_DEBUG("lastmq broker empty, m_sendWhichQueue:%d, pos:%d",
-                    m_sendWhichQueue.load(boost::memory_order_acquire), pos);
+                    m_sendWhichQueue.load(std::memory_order_acquire), pos);
           mq_index = pos;
           MQMessageQueue mq = m_queues.at(pos);
           string key = mq.getBrokerName() + UtilAll::to_string(mq.getQueueId());
@@ -244,19 +231,19 @@ class TopicPublishInfo {
   }
 
  private:
-  boost::mutex m_queuelock;
+  std::mutex m_queuelock;
   typedef vector<MQMessageQueue> QueuesVec;
   QueuesVec m_queues;
   typedef map<string, MQMessageQueue> MQMAP;
   MQMAP m_onSerivceQueues;
   MQMAP m_nonSerivceQueues;
-  boost::atomic<int> m_sendWhichQueue;
+  std::atomic<int> m_sendWhichQueue;
   map<MQMessageQueue, int64> m_brokerTimerMap;
-  boost::asio::io_service m_async_ioService;
-  boost::scoped_ptr<boost::thread> m_async_service_thread;
+
+  scheduled_thread_pool_executor m_scheduledExecutorService;
 };
 
 //<!***************************************************************************
-}  //<!end namespace;
+}  // namespace rocketmq
 
 #endif
