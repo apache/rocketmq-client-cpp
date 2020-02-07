17350c144c4db0297ebc73f55c03db33c032e610
ShannonDing <libya_003@163.com>
2020-02-07 20:24:40 +0800

    feat(package): add dpkg package script

--- dep/build.sh
+++ dep/build.sh
@@ -11,4 +11,5 @@ cp -R ${BASEDIR}/../include/*              ${BASEDIR}/rocketmq_amd64/usr/local/i
 cp ${BASEDIR}/../bin/librocketmq.so      ${BASEDIR}/rocketmq_amd64/usr/local/lib/
 cp ${BASEDIR}/../bin/librocketmq.a      ${BASEDIR}/rocketmq_amd64/usr/local/lib/
 
-dpkg-deb --build ${BASEDIR}/rocketmq_amd64
+VERSION=`cat ${BASEDIR}/rocketmq_amd64/DEBIAN/control | grep Version | awk -F ':' '{print $2}'| sed 's/^ *//'`
+dpkg-deb --build ${BASEDIR}/rocketmq_amd64 rocketmq_${VERSION}_amd64.deb
