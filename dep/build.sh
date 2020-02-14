820006e80aa6f888fdb0d7e955e6a4fdaee9223f
dinglei <libya_003@163.com>
2020-02-14 10:10:34 +0800

    update package script (#239)

--- dep/build.sh
+++ dep/build.sh
@@ -1,15 +1,15 @@
 BASEDIR=$(dirname "$0")
-if [[ ! -d ${BASEDIR}/rocketmq_amd64/usr/local/include/ ]]; then
-    mkdir -p ${BASEDIR}/rocketmq_amd64/usr/local/include/
+if [[ ! -d ${BASEDIR}/rocketmq_amd64/usr/local/include/rocketmq ]]; then
+    mkdir -p ${BASEDIR}/rocketmq_amd64/usr/local/include/rocketmq
 fi
 
 if [[ ! -d ${BASEDIR}/rocketmq_amd64/usr/local/lib ]]; then
     mkdir -p ${BASEDIR}/rocketmq_amd64/usr/local/lib
 fi
 
-cp -R ${BASEDIR}/../include/*              ${BASEDIR}/rocketmq_amd64/usr/local/include/
+cp -R ${BASEDIR}/../include/*              ${BASEDIR}/rocketmq_amd64/usr/local/include/rocketmq
 cp ${BASEDIR}/../bin/librocketmq.so      ${BASEDIR}/rocketmq_amd64/usr/local/lib/
 cp ${BASEDIR}/../bin/librocketmq.a      ${BASEDIR}/rocketmq_amd64/usr/local/lib/
 
 VERSION=`cat ${BASEDIR}/rocketmq_amd64/DEBIAN/control | grep Version | awk -F ':' '{print $2}'| sed 's/^ *//'`
-dpkg-deb --build ${BASEDIR}/rocketmq_amd64 rocketmq_${VERSION}_amd64.deb
+dpkg-deb --build ${BASEDIR}/rocketmq_amd64 rocketmq-client-cpp-${VERSION}.amd64.deb
