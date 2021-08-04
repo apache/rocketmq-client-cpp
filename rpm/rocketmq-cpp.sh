f0a8c6596291fd462db326b47a25d0abafb49f1f
Li Zhanhui <lizhanhui@gmail.com>
2021-08-04 16:38:33 +0800

    Remove unused files

--- rpm/rocketmq-cpp.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export CC=/usr/local/gcc-4.9.2/bin/gcc
-export CXX=/usr/local/gcc-4.9.2/bin/g++
-export LD_LIBRARY_PATH=/usr/local/gcc-4.9.2/lib64/
-export http_proxy=http://30.57.177.111:3128
-export https_proxy=http://30.57.177.111:3128
