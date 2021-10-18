a0db0cbea37203cc7cb5cd39a5098953840e3939
Li Zhanhui <lizhanhui@gmail.com>
2021-10-18 12:06:47 +0800

    BugFix: Add copyright file header comment

--- tools/fix_copyright.py
+++ tools/fix_copyright.py
@@ -2,36 +2,57 @@
 import os
 import fnmatch
 
-def checkAndFixFileHeaderComment(fileName, template):
+def checkAndFixFileHeaderComment(fileName, template, bazel=False):
     lines = []
-    with open(fileName, 'rw') as f:
+    with open(fileName, 'r') as f:
         lines = f.readlines()
-    
-    print(template)
-    for line in lines:
-        print(line)
 
+        if len(lines) > 0:
+            if bazel:
+                if lines[0].startswith("#"):
+                    return False
+            else:
+                if lines[0].startswith("/*"):
+                    return False
+        
+        lines.insert(0, template)
+    
+    with open(fileName, 'w') as f:
+        for line in lines:
+            f.write(line)
+    return True
 
 
 def main():
-    template = "/*\n"
-
+    bzl_template = "#" + os.linesep
+    template = "/*" + os.linesep
     with open('tools/copyright.txt', 'r') as reader:
         line = reader.readline()
         while line != '':
            template += " * " + line
+           bzl_template += "# " + line
            line = reader.readline()
-    template += " */"
+    template += " */" + os.linesep
+    bzl_template += "#" + os.linesep
 
+    cnt = 0
     for root, dir, files in os.walk(os.curdir):
         if ".git/" in root:
             continue
 
         for item in fnmatch.filter(files, "*.h"):
-            checkAndFixFileHeaderComment(root + os.sep + item, template)
+            if(checkAndFixFileHeaderComment(root + os.sep + item, template)):
+                cnt = cnt +1
 
         for item in fnmatch.filter(files, "*.cpp"):
-            checkAndFixFileHeaderComment(root + os.sep + item, template)
+            if (checkAndFixFileHeaderComment(root + os.sep + item, template)):
+                cnt = cnt + 1
+
+        for item in fnmatch.filter(files, "*.bazel"):
+            if (checkAndFixFileHeaderComment(root + os.sep + item, bzl_template, bazel=True)):
+                cnt = cnt + 1
+
+    print("{} files fixed".format(cnt))
 
 
 if __name__ == "__main__":
